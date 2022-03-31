07866df5503ef70a31f29746ba2693b49fb5be8c
Simeon Cheeseman <simeon@tablecheck.com>
2022-03-31 11:03:52 +0900

    add example app with bug

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,40 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+const {
+  createCypressModel,
+  simpleExecutePlan,
+  createCypressMachine,
+} = require("xstate-test-cypress");
+
+const model = createCypressModel(
+  createCypressMachine(
+    {
+      initial: "init",
+      states: {
+        init: { on: { OPEN: 'modalOpen' } },
+        modalOpen: { on: { TYPE: "withValues" } },
+        withValues: {},
+      },
+    },
+    {
+      withValues: () => {
+        cy.get("#name").should("have.value", "first check");
+        cy.get("#username").should("have.value", "second check");
+      },
+    }
+  ),
+  {
+    OPEN: () => {
+      cy.get('button').click()
+    },
+    TYPE: () => {
+      cy.get("#name").clear().type("first check");
+      cy.get("#username").clear().type("second check");
+    },
+  }
+);
+
+describe("page", () => {
+  simpleExecutePlan(model.getSimplePathPlansTo('withValues'), () => {
+    cy.visit("http://localhost:3000");
+  });
+});
